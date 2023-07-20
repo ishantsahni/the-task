d350aa7f0d50fa3acef8a51c792dfc96c76fe05d
Ishant Sahni <ishant.sahni@geminisolutions.com>
2023-07-20 18:51:41 +0530

    margin top added

--- src/components/TaskHeader/index.js
+++ src/components/TaskHeader/index.js
@@ -5,8 +5,9 @@ import { AiOutlineDown, AiOutlineDownSquare } from "react-icons/ai";
 import { BsThreeDots } from "react-icons/bs";
 
 const TaskHeader = () => {
-    return (
-        <div className="flex ishant w-full justify-between">
+  return (
+    <div className="flex ishant w-full justify-between">
+      <div className="flex w-2/5 justify-between">
         <div className="flex">
           <Checkbox
             //    {...label}
@@ -15,26 +16,29 @@ const TaskHeader = () => {
             defaultChecked
           />
           <div>
-            <p className="font-semibold text-[0.7rem] text-nero">ZTP_suresh-cdc_6782530715194866100</p>
+            <p className="font-semibold text-[0.7rem] text-nero">
+              ZTP_suresh-cdc_6782530715194866100
+            </p>
             <p className="text-[0.7rem] text-nero">Description Obj</p>
-          </div>
-          <div className="flex items-center">
-            <img
-              alt="success"
-              className="h-3 w-3 mr-1"
-              src={Resources.images.mainPage.success}
-            />
-            <p className="font-semibold text-[0.7rem] text-nero">Online</p>
           </div>
         </div>
         <div className="flex">
-          <BsThreeDots className="fill-darkSkyBlue" />
-          <div className="h-4 w-4">
-            <AiOutlineDownSquare className="fill-darkSkyBlue !border-0 rounded-2xl" />
-          </div>
+          <img
+            alt="success"
+            className="h-3 w-3 mr-1 mt-0.5"
+            src={Resources.images.mainPage.success}
+          />
+          <p className="font-semibold text-[0.7rem] text-nero">Online</p>
         </div>
       </div>
-    );
-}
+      <div className="flex">
+        <BsThreeDots className="fill-darkSkyBlue" />
+        <div className="h-4 w-4">
+          <AiOutlineDownSquare className="fill-darkSkyBlue !border-0 rounded-2xl" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TaskHeader;
