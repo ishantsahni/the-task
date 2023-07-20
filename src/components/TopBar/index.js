a683e6d4b84f117cd4be001a1ad5d73c1f1f3a6a
Ishant Sahni <ishant.sahni@geminisolutions.com>
2023-07-21 03:07:41 +0530

    margin bottom added

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const TopBar = () => {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center mb-4">
       <div>
         <Button className="!h-7" size="small" variant="contained" sx={{textTransform: "none"}}>Create Server</Button>
         <Button size="small" variant="outlined" sx={{textTransform: "none"}} className="!text-black !border-black !mx-4 !h-7">
