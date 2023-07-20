48619282747a9fd0c4b5f5ad09c19741c36975d9
Ishant Sahni <ishant.sahni@geminisolutions.com>
2023-07-21 02:48:49 +0530

    border color added

--- src/components/TaskItem/index.js
+++ src/components/TaskItem/index.js
@@ -5,7 +5,7 @@ import TaskInfo from "../TaskInfo";
 const TaskItem = ({items}) => {
   const [taskInfo, setTaskInfo] = useState(false);
   return (
-    <div className="bg-white border-l-2 border-red-600 mb-4 py-3">
+    <div className={`bg-white border-l-2 ${items.status === "online" ? "border-green-500" : "border-red-600"} mb-4 py-3`}>
       <div className="w-[97%] mx-auto">
         <TaskHeader items={items} taskInfo={taskInfo} setTaskInfo={setTaskInfo}/>
         <TaskInfo items={items} taskInfo={taskInfo} />
