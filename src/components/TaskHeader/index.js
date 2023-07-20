3a902bc3c4302b561e9d7cf650412660d082ffdc
Ishant Sahni <ishant.sahni@geminisolutions.com>
2023-07-21 01:32:30 +0530

    added item height

--- src/components/TaskHeader/index.js
+++ src/components/TaskHeader/index.js
@@ -1,10 +1,29 @@
 import { Checkbox } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { Resources } from "../../config/Resources";
-import { AiOutlineDown, AiOutlineDownSquare, AiOutlineUpSquare } from "react-icons/ai";
+import {
+  AiOutlineDown,
+  AiOutlineDownSquare,
+  AiOutlineUpSquare,
+} from "react-icons/ai";
 import { BsThreeDots } from "react-icons/bs";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
-const TaskHeader = ({taskInfo, setTaskInfo, items}) => {
+const options = ["Remove", "Edit"];
+
+const ITEM_HEIGHT = 48;
+
+const TaskHeader = ({ taskInfo, setTaskInfo, items }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className="flex ishant w-full justify-between">
       <div className="flex w-2/5 justify-between">
@@ -28,13 +47,47 @@ const TaskHeader = ({taskInfo, setTaskInfo, items}) => {
             className="h-3 w-3 mr-1 mt-0.5"
             src={Resources.images.mainPage.success}
           />
-          <p className="font-semibold text-[0.7rem] text-nero">{items.status}</p>
+          <p className="font-semibold text-[0.7rem] text-nero">
+            {items.status}
+          </p>
         </div>
       </div>
       <div className="flex">
-        <BsThreeDots className="fill-darkSkyBlue mr-3.5" />
-        <div className="h-4 w-4 cursor-pointer" onClick={() => setTaskInfo(!taskInfo)}>
-          {taskInfo ? <AiOutlineUpSquare className="fill-darkSkyBlue !border-0 rounded-2xl" /> : <AiOutlineDownSquare className="fill-darkSkyBlue !border-0 rounded-2xl" />}
+        <Menu
+          id="long-menu"
+          MenuListProps={{
+            "aria-labelledby": "long-button",
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          PaperProps={{
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: "100px",
+            },
+          }}
+        >
+          {options.map((option) => (
+            <MenuItem
+              key={option}
+              selected={option === "Pyxis"}
+              onClick={handleClose}
+            >
+              {option}
+            </MenuItem>
+          ))}
+        </Menu>
+        <BsThreeDots onClick={(e) => handleClick(e)} className="fill-darkSkyBlue mr-3.5" />
+        <div
+          className="h-4 w-4 cursor-pointer"
+          onClick={() => setTaskInfo(!taskInfo)}
+        >
+          {taskInfo ? (
+            <AiOutlineUpSquare className="fill-darkSkyBlue !border-0 rounded-2xl" />
+          ) : (
+            <AiOutlineDownSquare className="fill-darkSkyBlue !border-0 rounded-2xl" />
+          )}
         </div>
       </div>
     </div>
