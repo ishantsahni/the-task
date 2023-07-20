23f2ea723c504f5189af55203c9f99e06aa38d32
Ishant Sahni <ishant.sahni@geminisolutions.com>
2023-07-20 16:46:48 +0530

    added text field

--- src/components/TasksBox/index.js
+++ src/components/TasksBox/index.js
@@ -3,12 +3,14 @@ import TopBar from "../TopBar";
 import TaskItem from "../TaskItem";
 
 const TasksBox = () => {
-    return (
-        <div className="bg-slateGrey">
-            <TopBar />
-            <TaskItem />
-        </div>
-    )
+  return (
+    <div className="bg-slateGrey pt-5">
+      <div className="w-11/12 mx-auto">
+        <TopBar />
+        <TaskItem />
+      </div>
+    </div>
+  );
 };
 
 export default TasksBox;
--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -1,11 +1,37 @@
+import { Button, TextField } from "@mui/material";
 import React from "react";
 
 const TopBar = () => {
-    return (
-        <div>
-            <p>TopBar</p>
-        </div>
-    );
-}
+  return (
+    <div className="flex justify-between items-center">
+      <div>
+        <Button variant="contained" sx={{textTransform: "none"}}>Create Server</Button>
+        <Button variant="outlined" sx={{textTransform: "none"}} className="!text-black !border-black !mx-4">
+          Edit
+        </Button>
+        <Button variant="outlined" sx={{textTransform: "none"}} className="!text-black !border-black">
+          Remove
+        </Button>
+      </div>
+      <div>
+        <TextField
+          className="bg-slateGrey"
+          InputProps={{
+            style: {
+              borderRadius: "33px",
+              borderWidth: "0px",
+              borderColor: "black"
+            },
+          }}
+          autoComplete="off"
+          placeholder="Search by name.."
+          id="outlined-basic"
+          label=""
+          variant="outlined"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default TopBar;
