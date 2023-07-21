80912cf32b39fa6c834d9ed1de9631c300f5623e
Ishant Sahni <ishant.sahni@geminisolutions.com>
2023-07-21 16:57:32 +0530

    added formik

--- src/components/ServerForm/index.js
+++ src/components/ServerForm/index.js
@@ -70,28 +70,46 @@ const ServerForm = () => {
           className="bg-slateGrey"
           autoComplete="off"
           placeholder="Enter Name"
-          id="outlined-basic"
-          label="Name"
+          //   id="outlined-basic"
+          label=""
           variant="outlined"
+          id="name"
+          name="name"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.name}
         />
         <InputLabel id="demo-simple-select-label">Description</InputLabel>
         <TextField
           className="bg-slateGrey"
           autoComplete="off"
           placeholder="On-Prem Host description(256 character maximum)"
-          id="outlined-basic"
-          label="Description"
+          //   id="outlined-basic"
+          label=""
           variant="outlined"
+          id="description"
+          name="description"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.description}
         />
         <InputLabel id="demo-simple-select-label">NAT Space</InputLabel>
         <Select
           // labelId="demo-simple-select-label"
           // id="demo-simple-select"
           className="bg-slateGrey"
-          value={natSpace}
+          //   value={natSpace}
           label=""
           // placeholder="Select NAT Space"
-          onChange={handleChange}
+          //   onChange={handleChange}
+          id="spaceId"
+          name="spaceId"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.spaceId}
         >
           {items.map((item) => (
             <MenuItem key={item.id} value={item.name}>
@@ -104,10 +122,17 @@ const ServerForm = () => {
           className="bg-slateGrey"
           autoComplete="off"
           placeholder="Enter IP Address"
-          id="outlined-basic"
-          label="IP Address"
+          //   id="outlined-basic"
+          label=""
           variant="outlined"
+          id="addressId"
+          name="addressId"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.addressId}
         />
+        <button type="submit">Submit</button>
       </div>
     </form>
   );
