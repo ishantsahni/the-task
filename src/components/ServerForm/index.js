a69e62f86c10b1f55e6b2810c79edce5d82b2bcc
Ishant Sahni <ishant.sahni@geminisolutions.com>
2023-07-21 11:17:41 +0530

    text field added

--- src/components/ServerForm/index.js
+++ src/components/ServerForm/index.js
@@ -1,9 +1,34 @@
+import { TextField } from "@mui/material";
+
 const ServerForm = () => {
-    return (
-        <div>
-            <p>Server Form</p>
-        </div>
-    );
-}
+  return (
+    <div className="flex flex-col">
+      <TextField
+        className="bg-slateGrey"
+        autoComplete="off"
+        placeholder="Enter Name"
+        id="outlined-basic"
+        label="Name"
+        variant="outlined"
+      />
+      <TextField
+        className="bg-slateGrey"
+        autoComplete="off"
+        placeholder="On-Prem Host description(256 character maximum)"
+        id="outlined-basic"
+        label="Description"
+        variant="outlined"
+      />
+      <TextField
+        className="bg-slateGrey"
+        autoComplete="off"
+        placeholder="Enter IP Address"
+        id="outlined-basic"
+        label="IP Address"
+        variant="outlined"
+      />
+    </div>
+  );
+};
 
 export default ServerForm;
