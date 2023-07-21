f3c36deb371837a3b94a9cdb15ab6f65b16e6ddb
Ishant Sahni <ishant.sahni@geminisolutions.com>
2023-07-21 11:01:36 +0530

    modal added

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -1,11 +1,54 @@
-import { Button, TextField } from "@mui/material";
-import React from "react";
+import { Box, Button, Modal, TextField } from "@mui/material";
+import React, { useState } from "react";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  height: 200,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
 
 const TopBar = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <div className="flex justify-between items-center mb-4">
+      <Modal
+        open={modalOpen}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div className="flex flex-col justify-around h-full">
+            <div className="mx-auto">
+              <p className="font-mulish">
+                You are not authorized to access this service.
+              </p>
+            </div>
+            <div className="flex justify-center">
+              <Button
+                className="cursor-pointer"
+                variant="contained"
+                onClick={() => handleCloseModal()}
+              >
+                Close
+              </Button>
+            </div>
+          </div>
+        </Box>
+      </Modal>
       <div>
-        <Button className="!h-7" size="small" variant="contained" sx={{textTransform: "none"}}>Create Server</Button>
+        <Button onClick={() => setModalOpen(true)} className="!h-7" size="small" variant="contained" sx={{textTransform: "none"}}>Create Server</Button>
         <Button size="small" variant="outlined" sx={{textTransform: "none"}} className="!text-black !border-black !mx-4 !h-7">
           Edit
         </Button>
