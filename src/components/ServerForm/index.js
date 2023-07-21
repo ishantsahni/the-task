30fd60bcf02014680ed442cf013bcc5a7ee940e2
Ishant Sahni <ishant.sahni@geminisolutions.com>
2023-07-21 16:52:04 +0530

    form tag added

--- src/components/ServerForm/index.js
+++ src/components/ServerForm/index.js
@@ -63,51 +63,53 @@ const ServerForm = () => {
   });
 
   return (
-    <div className="flex flex-col">
-      <InputLabel id="demo-simple-select-label">Name</InputLabel>
-      <TextField
-        className="bg-slateGrey"
-        autoComplete="off"
-        placeholder="Enter Name"
-        id="outlined-basic"
-        label="Name"
-        variant="outlined"
-      />
-      <InputLabel id="demo-simple-select-label">Description</InputLabel>
-      <TextField
-        className="bg-slateGrey"
-        autoComplete="off"
-        placeholder="On-Prem Host description(256 character maximum)"
-        id="outlined-basic"
-        label="Description"
-        variant="outlined"
-      />
-      <InputLabel id="demo-simple-select-label">NAT Space</InputLabel>
-      <Select
-        // labelId="demo-simple-select-label"
-        // id="demo-simple-select"
-        className="bg-slateGrey"
-        value={natSpace}
-        label=""
-        // placeholder="Select NAT Space"
-        onChange={handleChange}
-      >
-        {items.map((item) => (
-          <MenuItem key={item.id} value={item.name}>
-            {item.name}
-          </MenuItem>
-        ))}
-      </Select>
-      <InputLabel id="demo-simple-select-label">IP Address</InputLabel>
-      <TextField
-        className="bg-slateGrey"
-        autoComplete="off"
-        placeholder="Enter IP Address"
-        id="outlined-basic"
-        label="IP Address"
-        variant="outlined"
-      />
-    </div>
+    <form onSubmit={formik.handleSubmit}>
+      <div className="flex flex-col">
+        <InputLabel id="demo-simple-select-label">Name</InputLabel>
+        <TextField
+          className="bg-slateGrey"
+          autoComplete="off"
+          placeholder="Enter Name"
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+        />
+        <InputLabel id="demo-simple-select-label">Description</InputLabel>
+        <TextField
+          className="bg-slateGrey"
+          autoComplete="off"
+          placeholder="On-Prem Host description(256 character maximum)"
+          id="outlined-basic"
+          label="Description"
+          variant="outlined"
+        />
+        <InputLabel id="demo-simple-select-label">NAT Space</InputLabel>
+        <Select
+          // labelId="demo-simple-select-label"
+          // id="demo-simple-select"
+          className="bg-slateGrey"
+          value={natSpace}
+          label=""
+          // placeholder="Select NAT Space"
+          onChange={handleChange}
+        >
+          {items.map((item) => (
+            <MenuItem key={item.id} value={item.name}>
+              {item.name}
+            </MenuItem>
+          ))}
+        </Select>
+        <InputLabel id="demo-simple-select-label">IP Address</InputLabel>
+        <TextField
+          className="bg-slateGrey"
+          autoComplete="off"
+          placeholder="Enter IP Address"
+          id="outlined-basic"
+          label="IP Address"
+          variant="outlined"
+        />
+      </div>
+    </form>
   );
 };
 
