6dca8014923804df5f037479bd691ae29e30dba8
Ishant Sahni <ishant.sahni@geminisolutions.com>
2023-07-21 11:32:57 +0530

    select added

--- src/components/ServerForm/index.js
+++ src/components/ServerForm/index.js
@@ -1,6 +1,16 @@
-import { TextField } from "@mui/material";
+import { MenuItem, Select, TextField } from "@mui/material";
+import { useState } from "react";
 
 const ServerForm = () => {
+  const [natSpace, setNatSpace] = useState("");
+
+  const handleChange = (event) => {
+    console.log("nat space ", event.target.value);
+    setNatSpace(event.target.value.name);
+  };
+
+  console.log("nat space ", natSpace);
+  
   return (
     <div className="flex flex-col">
       <TextField
@@ -19,6 +29,63 @@ const ServerForm = () => {
         label="Description"
         variant="outlined"
       />
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        className="bg-slateGrey"
+        value={natSpace}
+        label="NAT Space"
+        onChange={handleChange}
+      >
+        <MenuItem
+          value={{
+            id: "1",
+            name: "Box",
+          }}
+        >
+          Ten
+        </MenuItem>
+        <MenuItem
+          value={{
+            id: "12",
+            name: "DoS",
+          }}
+        >
+          DoS
+        </MenuItem>
+        <MenuItem
+          value={{
+            id: "13",
+            name: "Server Box",
+          }}
+        >
+          Server Box
+        </MenuItem>
+        <MenuItem
+          value={{
+            id: "14",
+            name: "Nat-vat",
+          }}
+        >
+          Nat-vat
+        </MenuItem>
+        <MenuItem
+          value={{
+            id: "15",
+            name: "Zth-vtc-1275844",
+          }}
+        >
+          Zth-vtc-1275844
+        </MenuItem>
+        <MenuItem
+          value={{
+            id: "17",
+            name: "GBD",
+          }}
+        >
+          GBD
+        </MenuItem>
+      </Select>
       <TextField
         className="bg-slateGrey"
         autoComplete="off"
